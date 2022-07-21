33e2b5cf52513a5b547b4e688b288540aa3c8b12
nikhilspace <nikhilmaharedu@gmail.com>
2022-07-21 18:37:47 +0530

    done

--- a.c
+++ a.c
@@ -1,126 +1,326 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<time.h>
+#include<stdlib.h>
 
-struct date {
-    int day,month,year;
+
+void main_menu();
+
+struct date
+{
+    int month,day,year;   
 };
 
-struct 
+struct
 {
-    char book[30];
-    char genre[10];
-    int book_id;
-    char borrower_name[30];
-    char status[10];
-    struct date last_update;
-} add,update,remov,check;
+    char item_name[30];
+    int item_code;
+    int price;
+    int quantity;
+    struct date update;
+    char customer[30];
+    long long int phone;
+    int total;
+    long long int invoice;
+}add,remov,edit,check;
 
-int delay(int input) 
+
+void delay (int n) 
 {
-    int ms = 800 * input;
+    int millisecond = 800*n;
 
-    clock_t start_time = clock();
+    time_t start = clock();
 
-    while (clock()<start_time + ms);
+    while (clock() < start + millisecond) {}
 }
 
-void ad() {
+
+void bill() 
+{
     system("cls");
-    FILE *old;
-    old=fopen("data.dat","a+");
+    FILE *old,*new,*bill;
+    old=fopen("inventory.dat","r");
+    new=fopen("newinventory.dat","w");
+    bill=fopen("Sales_report.dat","a+");
 
-    printf("Enter today's date (mm/dd/yy)\n:");
-    scanf("%d/%d/%d",&add.last_update.day,&add.last_update.month,&add.last_update.year);
-    printf("Book name \n:");
-    scanf("%s",&add.book);
-    printf("Genre \n:");
-    scanf("%s",add.genre);
-    printf("ISBN no. \n:");
-    scanf("%d",&add.book_id);
-    printf("Book status (Added/Borrowed/Returned) \n:");
-    scanf("%s",&add.status);
+    printf("Date (mm/dd/yy): ");
+    scanf("%d/%d/%d",&check.update.month,&check.update.day,&check.update.year);
+    printf("Customer Name: ");
+    scanf("%s",&check.customer);
+    printf("Phone: ");
+    scanf("%lld",&check.phone);
+    printf("Product code: ");
+    scanf("%d",&check.item_code);
+    printf("Quantity: ");
+    scanf("%d",&check.quantity);
+    int choice;
+    int rem=0;
+    printf("Enter 1 to add more product or 0 to continue: ");
+    scanf("%d",&choice);
+    if (choice==1) 
+    {   rem=1;
+        printf("Product code: ");
+        scanf("%d",&edit.item_code);
+            printf("Quantity: ");
+            scanf("%d",&edit.quantity);
+    }
+    printf("Enter 1 to add more product or 0 to continue: ");
+    scanf("%d",&choice);
+    if (choice==1) 
+    {   rem=2;
+        printf("Product code: ");
+        scanf("%d",&remov.item_code);
+            printf("Quantity: ");
+            scanf("%d",&remov.quantity);
+    }
 
-    fprintf(old,"%s %s %d %s %s %d/%d/%d \n",add.book,add.genre,add.book_id,add.status,add.borrower_name,add.last_update.day,add.last_update.month,add.last_update.year);
-    printf("Book added to the database!");
-    delay(2);
-    fclose(old);
-}
-
-void up () {
-    system("cls");
-    int count=0;
-    FILE *old,*new;
-    old=fopen("data.dat","r+");
-    new=fopen("newdata.dat","w+");
-
-    printf("Enter today's date (mm/dd/yy)\n:");
-    scanf("%d/%d/%d",&check.last_update.day,&check.last_update.month,&check.last_update.year);
-    printf("ISBN no. \n:");
-    scanf("%d",&check.book_id);
-
-    while(fscanf(old,"%s %s %d %s %s %d/%d/%d \n",add.book,add.genre,&add.book_id,add.status,add.borrower_name,&add.last_update.day,&add.last_update.month,&add.last_update.year)!=EOF) 
+    while (fscanf(old,"%s %d %d %d %d/%d/%d\n",add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
     {
-        if (check.book_id==add.book_id) 
+        if(check.item_code==add.item_code) 
+        {
+            add.total=add.price;
+            while (fscanf(old,"%s %d %d %d %d/%d/%d\n",add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
+                {
+                    if (edit.item_code==add.item_code) 
+                    {    
+                        add.total=add.total+add.price;
+                        while (fscanf(old,"%s %d %d %d %d/%d/%d\n",add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
+                            {
+                                if (remov.item_code==add.item_code) 
+                                {    
+                                 add.total=add.total+add.price;
+
+                                }
+                            }
+                    }
+                }
+        }   
+    }
+    printf("%d",add.total);
+}
+
+
+int remove_product()
+{   int choice;
+    system("cls");
+    FILE *old,*new;
+    old=fopen("inventory.dat","r");
+    new=fopen("newinventory.dat","w");
+    printf("Enter product code which you want to remove: ");
+    scanf("%d",&remov.item_code);
+
+    while (fscanf(old,"%s %d %d %d %d/%d/%d\n",add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
+    {
+        if (remov.item_code==add.item_code)
         {   
-            count=1;
-            system("cls");
-            printf("Book:%s\n",add.book);
-            printf("Enter status \n:");
-            scanf("%s",&check.status);
-            printf("Enter borrower name \n:");
-            scanf("%s",&check.borrower_name);
-            
-            fprintf(new,"%s %s %d %s %s %d/%d/%d \n",add.book,add.genre,add.book_id,check.status,check.borrower_name,check.last_update.day,check.last_update.month,check.last_update.year);  
-        }        
-        else {
-            fprintf(new,"%s %s %d %s %s %d/%d/%d \n",add.book,add.genre,add.book_id,add.status,add.borrower_name,add.last_update.day,add.last_update.month,add.last_update.year);
+            choice=1;
+            printf("\n\n\nProduct Deleted!\n");
         }
+        else
+        fprintf(new,"%s %d %d %d %d/%d/%d\n",add.item_name,add.item_code,add.price,add.quantity,add.update.month,add.update.day,add.update.year);
     }
 
-    if (count!=1) 
-    {   
-        int choice;
-        printf("book not found \n");
-        printf("enter 1 to try again or 0 to exit");
-        scanf("%d",&choice);
-
-        if (choice==1) {
-            up();
-        }
-        else 
-        goto exit;
-
-    }
-    exit:
     fclose(old);
     fclose(new);
-    remove("data.dat");
-    rename("newdata.dat","data.dat");
+    remove("inventory.dat");
+    rename("newinventory.dat","inventory.dat");
+
+    if (choice==1)
+    {
+        int try;
+        printf("\n\nEnter 1 to delete another product or 0 to exit:\n");
+        scanf("%d",&try);
+    
+    if (try==1) {
+        remove_product();
+    }
+    else
+        return 0;
+    }
+
+    if (choice!=1);
+    {
+        printf("Product not in inventory\n\n");
+        int try;
+        printf("Enter 1 to try again or 0 to exit\n:");
+        scanf("%d",&try);
+    
+        if (try==1) 
+        {
+        remove_product();
+        }
+        else
+        return 0;
+    }
+
 }
-int main_menu() {
+
+void check_inventory() 
+{
+    system("cls");
+    FILE *old;
+    old=fopen("inventory.dat","r");
+    
+    printf("PRODUCT|PRODUCT CODE|PRICE|QUANTITY|LAST UPDATED\n\n");
+    while (fscanf(old,"%s %d %d %d %d/%d/%d\n",add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
+    {
+        printf("%s  %d  %drs  %d  %d/%d/%d\n",add.item_name,add.item_code,add.price,add.quantity,add.update.month,add.update.day,add.update.year);
+    }
+}
+
+
+void edit_product() 
+{   int count;
+    system("cls");
+    FILE *old,*new;
+    old=fopen("inventory.dat","r");
+    new=fopen("newinventory.dat","w");
+    printf("Today's Date (mm/dd/yy): \n");
+    scanf("%d/%d/%d",&edit.update.day,&edit.update.month,&edit.update.year);
+    printf("Enter Product code: ");
+    scanf("%d",&check.item_code);
+
+    while (fscanf(old,"%s %d %d %d %d/%d/%d\n",add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
+    {
+        if (check.item_code==add.item_code) {
+            try_again:
+            count=1;
+            int choice;
+            system("cls");
+            printf("1.Change Name\n2.Change Price\n3.Change Quantity\n4.Main Menu\n");
+            scanf("%d",&choice);
+
+            if (choice==1) 
+            {
+                printf("Enter new name: ");
+                scanf("%s",edit.item_name);
+
+                fprintf(new,"%s %d %d %d %d/%d/%d\n",edit.item_name,add.item_code,add.price,add.quantity,edit.update.month,edit.update.day,edit.update.year);
+                printf("Edited succesfully.\n");
+
+            }
+
+            else if (choice==2) 
+            {
+                printf("Enter new price: ");
+                scanf("%d",&edit.price);
+
+                fprintf(new,"%s %d %d %d %d/%d/%d\n",add.item_name,add.item_code,edit.price,add.quantity,edit.update.month,edit.update.day,edit.update.year);
+                printf("Edited succesfully.\n");
+
+            }
+
+            else if (choice==3) 
+            {
+                printf("Enter quantity: ");
+                scanf("%d",&edit.quantity);
+
+                fprintf(new,"%s %d %d %d %d/%d/%d\n",add.item_name,add.item_code,add.price,edit.quantity,edit.update.month,edit.update.day,edit.update.year);
+                printf("Edited succesfully.\n");
+
+            }
+            else if (choice!=1||choice!=2||choice!=3)
+            {
+                printf("Wrong Input!\n");
+                delay(3);
+                goto try_again;
+            }
+        }
+        else
+        fprintf(new,"%s %d %d %d %d/%d/%d\n",add.item_name,add.item_code,add.price,add.quantity,add.update.month,add.update.day,add.update.year);
+    }
+        if (count!=1) 
+        {
+            int n;
+            printf("Product not in database.\n");
+            printf("Enter 1 to try again or 2 for Main menu.\n\n:");
+                scanf("%d",&n);
+                if (n==1)
+                edit_product();
+
+                else
+                main_menu();
+        }
+        fclose(old);
+        fclose(new);
+        remove("inventory.dat");
+        rename("newinventory.dat","inventory.dat");
+}
+
+
+void add_product() 
+{
+    system("cls");
+    FILE *old;
+    old=fopen("inventory.dat","a+");
+    try_again:
+
+    printf("Product Name: \n");
+    scanf("%s",check.item_name);
+    printf("Product Code: \n");
+    scanf("%d",&check.item_code);
+
+    while (fscanf(old,"%s %d %d %d %d/%d/%d\n",&add.item_name,&add.item_code,&add.price,&add.quantity,&add.update.month,&add.update.day,&add.update.year)!=EOF)
+    {
+        if (check.item_code==add.item_code) 
+        {
+            printf("Product already in inventory.\n");
+            printf("Enter 1 to input another product, 2 to edit existing product details or 0 for Main menu.\n\n:");
+            int choice;
+            scanf("%d",&choice);
+
+            if (choice==1)
+            goto try_again;
+
+            else if (choice==2)
+            edit_product();
+
+            else
+            main_menu();
+        }
+    }
+    
+    printf("Product MRP: \n");
+    scanf("%d",&add.price);
+    printf("Product Quantity: \n");
+    scanf("%d",&add.quantity);
+    printf("Today's Date (mm/dd/yy): \n");
+    scanf("%d/%d/%d",&add.update.day,&add.update.month,&add.update.year);
+
+    fprintf(old,"%s %d %d %d %d/%d/%d\n",check.item_name,check.item_code,add.price,add.quantity,add.update.day,add.update.month,add.update.year);
+
+    fclose(old);
+}
+
+
+void main_menu () 
+{
     int choice;
     system("cls");
-    printf("\t\t\t:::MAIN MENU:::\n\n\n");
-    
-    printf("1.Add new book to library \n2.Change book status\n\n\nENTER:");
+    printf("\t\t\t---MAIN MENU---");
+    printf("\n\n1.Print bill\n2.Add Product\n3.Edit Product\n4.Check Inventory\n5.Remove Product\n\n\n:");
     scanf("%d",&choice);
 
     switch (choice) {
-    case 1:ad();break;
-    case 2:up();break;
-    default:break;
+        case 1:bill();break;
+        case 2:add_product();break;
+        case 3:edit_product();break;
+        case 4:check_inventory();break;
+        case 5:remove_product();break;
     }
 }
 
-void main() {
+void main () 
+{
     system("cls");
-    printf("\t\tCOLLEGE LIBRARY MANAGEMENT PROGRAM \n");
-    printf("\t\tby Nikhil Mahar \n");
-    for (int i=0;i<=3;i++) {
-        delay(1);
-        printf(".");
-    }
-    
+    printf("\t\t\tFASHION STORE MANAGEMENT PROGRAM\n");
+    printf("\t\t\tby Nikhil Mahar");
+
+for(int i=0;i<3;i++) 
+{
+    //delay(1);
+    printf(".");
+}
     main_menu();
 }
+
+
