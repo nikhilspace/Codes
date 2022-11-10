7ad83c536265616e00f47fa49006cfa7a9520187
nikhilspace <nikhilmaharedu@gmail.com>
2022-11-10 14:37:19 +0530

    py

--- all/ImageClassificationwithNN.py
+++ all/ImageClassificationwithNN.py
@@ -8,8 +8,8 @@ from tensorflow import datasets, layers, models
 (training_images,training_label),(testing_images,testing_label) = datasets.cifar10.load_data()
 training_images, testing_images = training_images / 255, testing_images / 255
 
-class_names = ['plane','car','horse','deer','dog','cat','truck','frog','ship','bird']
 
+class_names = ['plane','car','horse','deer','dog','cat','truck','frog','ship','bird']
 
 for i in range(16):
     plt.subplot(4,4,i+1)
@@ -17,8 +17,8 @@ for i in range(16):
     plt.yticks([])
     plt.imshow(training_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[training_label[i][0]])
+    plt.show()
 
-plt.show()
 
 training_images = training_images[:20000]
 training_label = training_label[:20000]
@@ -35,6 +35,7 @@ model.add(layers.Conv2D(64, (3,3), activation = 'relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation = 'relu'))
 model.add(layers.Dense(10, activation = 'softmax'))
+
 
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', matrics=['accuracy'])
 model.fit(training_images,training_label, epochs=10,validation_data=(training_images,training_label))
