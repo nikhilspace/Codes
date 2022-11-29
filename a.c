57241b01662688bc992666d446f21667a1dc52f5
nikhilspace <nikhilmaharedu@gmail.com>
2022-11-29 19:22:42 +0530

    new

--- a.c
+++ a.c
@@ -1,15 +1,51 @@
 #include<stdio.h>
+#include<conio.h>
 
-void main() 
+void main()
 {
-    int table;
-    printf("enter any number you want find its table");
-    scanf("%d",&table);
 
-    for(int i=1;i<=10;i++)
-    {
-        int t = i*table;
-        printf("%d\n",t);
-    }
+int one=0,two=0;
+printf("Enter students percentage to check what their grade is:");
+scanf("%d",&one);
+
+if(one >= 95 && one <= 100)
+{
+    printf("The grade is A+");
 }
-
+else if(one >= 90 && one <= 94)
+{
+    printf("The grade is A");
+}
+else if(one >= 85 && one <= 89)
+{
+    printf("The grade is A-");
+}
+else if(one >= 80 && one <= 84)
+{
+    printf("The grade is B+");
+}
+else if(one >= 75 && one <= 79)
+{
+    printf("The grade is B");
+}
+else if(one >= 70 && one <= 74)
+{
+    printf("The grade is B-");
+}
+else if(one >= 65 && one <= 69)
+{
+    printf("The grade is C+");
+}
+else if(one >= 60 && one <= 64)
+{
+    printf("The grade is C");
+}
+else if(one > 100)
+{
+    printf("Enter result in percentile");
+}
+else
+{
+    printf("You have failed!!!");
+}
+}
